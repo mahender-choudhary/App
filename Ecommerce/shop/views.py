571b5ae8470cda93e1a984be857950d4a66b53f2
Mahendra Choudhary <myios7726@gmail.com>
2023-02-08 10:15:10 +0530

    4.2

--- Ecommerce/shop/views.py
+++ Ecommerce/shop/views.py
@@ -5,13 +5,11 @@ from math import ceil
 
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4) - (n//4))
-    params = {'no_of_slides': nSlides, 'range': range(nSlides), 'product': products}
-    return render(request, 'shop/index.html')
-
+    products= Product.objects.all()
+    n= len(products)
+    nSlides= n//4 + ceil((n/4) + (n//4))
+    params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
+    return render(request,"shop/index.html", params)
 def about(request):
     return render(request, 'shop/about.html')
 
