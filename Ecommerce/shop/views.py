9fbed9be2f0a48bf55323e6b56389a50c1e5a8e0
Mahendra Choudhary <myios7726@gmail.com>
2023-02-16 20:39:44 +0530

    1

--- Ecommerce/shop/views.py
+++ Ecommerce/shop/views.py
@@ -5,6 +5,7 @@ from math import ceil
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
     # products = Product.objects.all()
     # print(products)
@@ -23,23 +24,30 @@ def index(request):
     # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
     # allProds = [[products, range(1, nSlides), nSlides],
     #             [products, range(1, nSlides), nSlides]]
-    params = {'allProds':allProds}
+    params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
+
 
 def about(request):
     return render(request, 'shop/about.html')
 
+
 def contact(request):
     return render(request, 'shop/contact.html')
+
 
 def tracker(request):
     return render(request, 'shop/tracker.html')
 
+
 def search(request):
     return render(request, 'shop/search.html')
 
-def productView(request):
-    return render(request, 'shop/prodView.html')
+
+def productView(request, myid):
+    product=Product.objects.filter(id=myid)
+    return render(request, "shop/prodView.html", {'product': product})
+
 
 def checkout(request):
     return render(request, 'shop/checkout.html')
