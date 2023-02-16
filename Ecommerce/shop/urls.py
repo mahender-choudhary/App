de532ca6f4fe362b462944456fd12ff43ed96f30
Mahendra Choudhary <myios7726@gmail.com>
2023-02-16 15:42:58 +0530

    2.2

--- Ecommerce/shop/urls.py
+++ Ecommerce/shop/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("contact/", views.contact, name = "ContactUs"),
     path("tracker/", views.tracker, name = "TrackingStatus"),
     path("search/", views.search, name = "Search"),
-    path("productview/", views.productView, name = "Search"),
+    path("productView/", views.productView, name = "Search"),
     path("checkout/", views.checkout, name = "Checkout"),
 ]
